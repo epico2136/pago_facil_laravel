4192602dea39fcb4b4501c3aa0ba7256c9dd54ce
epico2136 <epicoramirez@gmail.com>
2020-02-22 11:44:59 -0500

    Aprendiendo arreglos

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,52 @@ Route::get('/', function () {
 });
 //Primera rut: mensaje
 Route::get('hola',function(){
-    echo "Hola madrugadores";
+    $ficha = 1753443;
+    echo 'Hola madrugadores',' ',  $ficha;
+    
+});
+
+//Ruta tratr arreglos
+Route::get('arreglo' , function(){
+
+    //crear arreglo
+    $estudiantes =["C"=>"Carlos",
+                   "A"=>"Ana",
+                   "L"=>"Lola",
+                   "N"=>"Nancy",
+                   "T"=>"Teresa",
+                   "S"=>"Sandra",
+                   "E"=>"Edwin",
+                   "P"=>"Pedro",
+                   "M"=>"Maria",
+                   "O"=>"Orlando",
+                   "W"=>"Wilson",
+                   "J"=>"Juan",];
+
+    //Agregar elementos a un arreglo
+    $estudiantes []="Fabio";
+
+    //var dump: analiza una variable
+    echo"<pre>";
+    var_dump($estudiantes);
+    echo"</pre>";
+    //imprimir cada valor del arreglo estudiantes
+    echo $estudiantes["C"];
+    echo "<hr/>";
+    echo $estudiantes["A"];
+    echo "<hr/>";
+    echo $estudiantes["L"];
+    echo "<hr/>";
+    echo "<br/>";
+    echo "<br/>";
+
+
+    //Imprimir un arreglo grande 
+    foreach($estudiantes as $clave => $estudiante){
+    echo $clave,"=",$estudiante;
+    echo "<hr/>";
+
+    }
 
 
 });
